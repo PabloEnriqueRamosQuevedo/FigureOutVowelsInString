e70b14b092d155dd6c2e548ecd7cbcd32fcd9148
PabloEscobarFromNova <pabloerq@gmail.com>
2016-10-22 16:26:24 -0400

    FindVowel
    This program has the steps for solving the problem outlined out their. Solve it.

---
+++ FindVowel.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class FindVowel
+{
+	public static void main (String [] args) // Just check if there is a vowel in the string.
+	{
+		Scanner keyboard = new Scanner(System.in);
+		String vowel_a = "a";
+		String vowel_e = "e";
+		String vowel_i = "i";
+		String vowel_o = "o";
+		String vowel_u = "u";
+
+		ordGiven.equals(""))
+/*  String s = "Strings are immutable";
+      char result = s.charAt(8);		// The first count start at zero. So, in cat, 0 is c, a is 1, t is 3.
+      System.out.println(result);
+*/
+		String userInput;  // Figure out what method from the the object keyboard goes here. I don't know what it is.
+		int stringLength = userInput.length-1; 	// Will give you the length of characters. ex. Cat will give a number 3.
+
+	// Use a loop where the condition is (stringLength >0)
+			// Each iteration use the charAt and compare it to the stringLength.
+
+			//Now, Check for each vowel using one if statement.
+			// If it does, end the loop and say that it is displayed.
+
+			//subtract the stringLength value.
+
+
+
+
+	}
+
+}
